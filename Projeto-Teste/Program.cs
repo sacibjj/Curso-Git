4d26d16e0f9e8b454cc5175892d801a2bbdc716c
Anderson Cardozo <guardeiromaster@gmail.com>
2020-06-10 16:42:31 -0300

    Programa inicializado, com a frase Ola Mundo

---
+++ Projeto-Teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projeto_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!!!");
+            Console.ReadLine();
+        }
+    }
+}
